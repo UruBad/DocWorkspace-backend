4de6227634cd1a487ad51268dad27a81ade9e01a
Konstantin <k.telyakov@uzum.com>
2024-08-01 13:11:05 +0300

    Start project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import helmet from 'helmet';
+import { ExpressAdapter } from '@nestjs/platform-express';
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  app.use(helmet());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
